5f9d7741d0230f3756d532dcff0443fb25d374a3
gl8429 <lucifer92.lin@gmail.com>
2015-09-20 00:42:46 -0500

    add port listener

--- src/Server.java
+++ src/Server.java
@@ -1,15 +1,63 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
 /**
  * Created by Lucifer on 9/19/15.
  */
 
-import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util;
-import java.io.*;
+public class Server extends Thread{
 
-public class Server {
     private static final String FILE_NAME = "/Users/Lucifer/Documents/ds/server.txt";
+    private ServerSocket serverSocket;
 
+    public Server(int port) throws IOException
+    {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000);
+    }
+
+    public void run()
+    {
+        while(true)
+        {
+            try
+            {
+                System.out.println("Waiting for client on port " +
+                        serverSocket.getLocalPort() + "...");
+                Socket server = serverSocket.accept();
+                System.out.println("Just connected to "
+                        + server.getRemoteSocketAddress());
+                DataInputStream in =
+                        new DataInputStream(server.getInputStream());
+                System.out.println(in.readUTF());
+                DataOutputStream out =
+                        new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for connecting to "
+                        + server.getLocalSocketAddress() + "\nGoodbye!");
+                server.close();
+            }catch(SocketTimeoutException s)
+            {
+                System.out.println("Socket timed out!");
+                break;
+            }catch(IOException e)
+            {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
     public static void main(String[] args) {
         NameTable nameTable = new NameTable(FILE_NAME);
-        System.out.println(nameTable.size());
+        int port = nameTable.getPort(0);
+        try {
+            Thread t = new Server(port);
+            t.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
