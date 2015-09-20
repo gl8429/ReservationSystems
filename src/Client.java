b4937a3653a5ec87b9e14a83f571403dcba5d998
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2015-09-20 00:20:04 -0500

    update server.txt

--- src/Client.java
+++ src/Client.java
@@ -9,19 +9,16 @@ import java.net.*;
 import java.io.*;
 
 public class Client {
-
-    private static final String FILE_NAME = "/Users/Lucifer/Documents/ds/server.txt";
+    private static final String FILE_NAME = "/Users/Xingyuan/Documents/GitHub/ReservationSystems/testCase/server.txt";
     private NameTable nameTable;
-    
 
     public NameTable getNameTable() {
         try {
-
             BufferedReader bf = new BufferedReader(new FileReader(new File(FILE_NAME)));
             String line;
             this.nameTable = new NameTable();
 
-            Util.println("\nStart loading Server information...");
+            Util.println("\nStart loading server information...");
             while ((line = bf.readLine()) != null) {
                 String[] serverInfo = line.split(" ");
                 String serverName = serverInfo[0];
@@ -42,8 +39,6 @@ public class Client {
 
     public static void main(String [] args)
     {
-        Server server = new Server();
-        NameTable nameTable = server.getNameTable();
 
         String serverName = args[0];
         int port = Integer.parseInt(args[1]);
