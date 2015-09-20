a99f29e5b04c4d2d3b21d4da650eabf5a73a7cf3
gl8429 <lucifer92.lin@gmail.com>
2015-09-19 23:15:21 -0500

    init

---
+++ src/NameTable.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Xingyuan on 9/19/15.
+ */
+
+public class NameTable {
+    final int maxSize = 100;
+    private final String[] names = new String[maxSize];
+    private final String[] hosts = new String[maxSize];
+    private final int[] ports = new int[maxSize];
+    private int size = 0;
+
+    public int search(String s) {
+        for (int i = 0; i < size; ++i) {
+            if (names[i].equals(s)) return i;
+        }
+        return -1;
+    }
+
+    public boolean insert(String s, String hostName, int portNumber) {
+        int oldIndex = search(s);   // Is it already there
+        if (oldIndex == -1 && size < maxSize) {
+            names[size] = s;
+            hosts[size] = hostName;
+            ports[size] = portNumber;
+            ++size;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public int getPort(int index) {
+        return ports[index];
+    }
+
+    public String getHostName(int index) {
+        return hosts[index];
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,41 @@
+/**
+ * Created by Lucifer on 9/19/15.
+ */
+
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Util;
+import java.io.*;
+
+public class Server {
+    private static final String FILE_NAME = "/Users/Lucifer/Documents/ds/server.txt";
+    private NameTable nameTable;
+
+    public NameTable getNameTable() {
+        try {
+
+            BufferedReader bf = new BufferedReader(new FileReader(new File(FILE_NAME)));
+            String line;
+            this.nameTable = new NameTable();
+
+            Util.println("\nStart loading Server information...");
+            while ((line = bf.readLine()) != null) {
+                String[] serverInfo = line.split(" ");
+                String serverName = serverInfo[0];
+                String serverHost = serverInfo[1];
+                int serverPort = Integer.parseInt(serverInfo[2]);
+                if (this.nameTable.insert(serverName, serverHost, serverPort)){
+                    Util.println(String.format("Successfully load server: %s", serverName));
+                } else {
+                    Util.println(String.format("%s has been loaded", serverName));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Util.println("End loading Server information...\n");
+        return this.nameTable;
+    }
+    public static void main(String[] args) {
+        Util.println("aa");
+    }
+
+}
