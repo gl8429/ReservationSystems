86b63df2424a56c93783cc33e2e268bc4c71fed2
Xingyuan Wang <xingyuan.wang.zju@gmail.com>
2015-09-20 21:10:49 -0500

    Add Seat

--- src/Seat.java
+++ src/Seat.java
@@ -1,24 +1,66 @@
 import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Created by Xingyuan on 9/20/15.
  */
 public class Seat {
+    private final Map<Integer, String> seat = new Hashtable<Integer, String>();
+
+    public Seat(int total) {
+        for(int i = 0; i < total; ++i) {
+            seat.put(i + 1, " ");
+        }
+    }
 
     int getLeftSeats() {
-        return 0;
+        int count = 0;
+        for(Map.Entry<Integer, String> entry : seat.entrySet()) {
+            if(entry.getValue().equals(" ")) {
+                ++count;
+            }
+        }
+        return count;
     }
 
     ArrayList<Integer> reserve(String name, int num) {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> assignedSeats = new ArrayList<Integer>();
+        for (Map.Entry<Integer, String> entry : seat.entrySet()) {
+            if (entry.getValue().equals(" ")) {
+                if (num == 0)
+                    break;
+                seat.put(entry.getKey(), name);
+                assignedSeats.add(entry.getKey());
+                --num;
+            }
+        }
+        return assignedSeats;
     }
 
     ArrayList<Integer> delete(String name) {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> deletedSeats = new ArrayList<Integer>();
+        for (Map.Entry<Integer, String> entry : seat.entrySet()) {
+            if (entry.getValue().equals(name)) {
+                deletedSeats.add(entry.getKey());
+                seat.put(entry.getKey(), " ");
+            }
+        }
+        return deletedSeats;
     }
 
     ArrayList<Integer> search(String name) {
-        return new ArrayList<Integer>();
+        ArrayList<Integer> reservedSeats = new ArrayList<Integer>();
+        for (Map.Entry<Integer, String> entry : seat.entrySet()) {
+            if (entry.getValue().equals(name)) {
+                reservedSeats.add(entry.getKey());
+            }
+        }
+        return reservedSeats;
     }
 
+    public static void main(String[] args) {
+        Seat seat = new Seat(100);
+        seat.search("Garry");
+    }
 }
