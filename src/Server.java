86fe8d7db8a072aa31bcefd5b06aa8b4b6359151
gl8429 <lucifer92.lin@gmail.com>
2015-09-20 19:57:22 -0500

    Update Server for multithread

--- src/Server.java
+++ src/Server.java
@@ -1,3 +1,5 @@
+
+
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -5,23 +7,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
 
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
+
 /**
  * Created by Lucifer on 9/19/15.
  */
 
 public class Server extends Thread{
 
-    private static final String FILE_NAME = "/Users/Lucifer/Documents/ds/server.txt";
+    private static final String FILE_NAME = "/Users/Lucifer/IdeaProjects/ReservationSystems/testCase/server.txt";
     private ServerSocket serverSocket;
+    private static final Semaphore semaphore = new Semaphore(1);
 
     public Server(int port) throws IOException
     {
         serverSocket = new ServerSocket(port);
-        serverSocket.setSoTimeout(10000);
+        serverSocket.setSoTimeout(100000);
     }
 
     public void run()
     {
+        int i = 0;
         while(true)
         {
             try
@@ -29,16 +37,10 @@ public class Server extends Thread{
                 System.out.println("Waiting for client on port " +
                         serverSocket.getLocalPort() + "...");
                 Socket server = serverSocket.accept();
-                System.out.println("Just connected to "
-                        + server.getRemoteSocketAddress());
-                DataInputStream in =
-                        new DataInputStream(server.getInputStream());
-                System.out.println(in.readUTF());
-                DataOutputStream out =
-                        new DataOutputStream(server.getOutputStream());
-                out.writeUTF("Thank you for connecting to "
-                        + server.getLocalSocketAddress() + "\nGoodbye!");
-                server.close();
+                Thread t = new Thread(new sellRunnable(server));
+                t.setName("Server: " + i++);
+                t.start();
+
             }catch(SocketTimeoutException s)
             {
                 System.out.println("Socket timed out!");
@@ -50,14 +52,68 @@ public class Server extends Thread{
             }
         }
     }
+
+    public class sellRunnable implements Runnable{
+
+        private Socket socket;
+
+        public sellRunnable(Socket socket) {
+            this.socket = socket;
+        }
+
+        public void run() {
+            try {
+                sell(socket);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sell(Socket server) throws IOException, InterruptedException {
+
+        while(!soldAll){
+            Thread.sleep(1000);
+            semaphore.acquire();
+            if (leftTickets > 0) {
+                System.out.println("Just connected to "
+                        + server.getRemoteSocketAddress());
+                DataInputStream in =
+                        new DataInputStream(server.getInputStream());
+                Thread t = Thread.currentThread();
+                String tmp = in.readUTF();
+                System.out.println(t.getName() + tmp);
+                //System.out.println(in.readUTF());
+
+
+
+
+                DataOutputStream out =
+                        new DataOutputStream(server.getOutputStream());
+                out.writeUTF("Thank you for connecting to "
+                        + server.getLocalSocketAddress() + "\nGoodbye!");
+            } else {
+                soldAll = true;
+                System.out.println("Sold out all tickets");
+            }
+
+            semaphore.release();
+            server.close();
+        }
+    }
+
     public static void main(String[] args) {
         NameTable nameTable = new NameTable(FILE_NAME);
-        int port = nameTable.getPort(0);
-        try {
-            Thread t = new Server(port);
-            t.start();
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < nameTable.size(); i++) {
+            int port = nameTable.getPort(0);
+            try {
+                Thread t = new Server(port);
+                t.start();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
